dc4d5cd3fb061d32a17149a83e1f6b3acdce1d37
nidanur ayyıldız <nidanurayyildiz.2@gmail.com>
2026-05-02 09:28:13 +0300

    Change executor to use SocialGroup instead of ProximityExecutor

--- src/utils/response.py
+++ src/utils/response.py
@@ -9,7 +9,7 @@ def build_response(context):
     output_groups   = OutputGroups(value=context.groups)
     outputs         = ProximityOutputs(outputGroups=output_groups)
     response        = ProximityResponse(outputs=outputs)
-    executor        = ConfigExecutor(value=ProximityExecutor(value=response))
+    executor        = ConfigExecutor(value=SocialGroup(value=response))
     package_configs = PackageConfigs(executor=executor)
     package         = PackageHelper(packageModel=PackageModel, packageConfigs=package_configs)
     return package.build_model(context)
